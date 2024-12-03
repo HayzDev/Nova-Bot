b4977a0bdc6b41404f438223245f928cb455e9d9
HayzDev <xanderbuhr714@gmail.com>
2024-12-02 23:41:19 -0800

    Started creating UI

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 import tkinter as tk
+import pyautogui
 
 load_dotenv()
 
@@ -9,12 +10,34 @@ client = OpenAI(
     api_key=os.getenv("OPENAI_API_KEY"),
 )
 
+window_width = 800
+window_height = 100
+
+screen_width, screen_height = pyautogui.size()
+
+pyautogui.moveTo(screen_width / 2, screen_height / 2)
+
 
 def main():
     root = tk.Tk()
 
-    label = tk.Label(root, text="Welcome to Nova-Bot")
-    label.pack()
+    window_pos = "+" + str(round(screen_width / 2 - 0.5 * window_width)) + "+" + str(round(screen_height / 2 - 0.5 * window_height))
+    root.geometry(window_pos)
+    root.geometry(str(window_width) + "x" + str(window_height))
+
+    entry_frame = tk.Frame(root)
+    entry_frame.grid(column=0, row=0)
+
+    entry = tk.Entry(entry_frame)
+    entry.grid(column=0, row=0)
+
+    def entry_submit(event=None):
+        print("Test")
+
+    entry.bind("<Return>", entry_submit)
+
+    button = tk.Button(entry_frame, text="Start", command=entry_submit, borderwidth=1)
+    button.grid(column=1, row=0)
 
     root.mainloop()
 
